0575cec9b7f4c80cdc073ac3178277fba1167594
satishwaghela <satishmwaghela@gmail.com>
2019-11-04 13:53:14 +0530

    Build using webpack for minify & uglify

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = (env, argv) => {
+  const SERVER_PATH = './src/bin/www.js';
+
+  return ({
+    entry: {
+      server: SERVER_PATH,
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/',
+      filename: '[name].js'
+    },
+    mode: argv.mode,
+    target: 'node',
+    node: {
+      // Need this when working with express, otherwise the build fails
+      __dirname: false,   // if you don't put this is, __dirname
+      __filename: false,  // and __filename return blank or /
+    },
+    externals: [nodeExternals()], // Need this to avoid error when working with Express
+    module: {
+      rules: [
+        {
+          // Transpiles ES6-8 into ES5
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  })
+}
+
+// Webpack 4 basic tutorial:
+// https://www.valentinog.com/blog/webpack-4-tutorial/#webpack_4_production_and_development_mode
+
+// Development mode is optimized for build speed and does nothing more than providing an un-minified bundle.
+// Production mode enables all sorts of optimizations like minification, scope hoisting, tree-shaking and more.
